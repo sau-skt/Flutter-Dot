adebecf425adadee2b4c4814d3f0df000c9321d4
sau-skt <saurabhchatterjee18@gmail.com>
2024-04-10 14:08:34 -0700

    SearchBar and Horizontal ListView

--- lib/features/authentication/screens/home/home.dart
+++ lib/features/authentication/screens/home/home.dart
@@ -1,7 +1,12 @@
+import 'package:e_commerse/utils/constants/colors.dart';
+import 'package:e_commerse/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
 import 'widgets/home_appbar.dart';
+import 'widgets/home_categories.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -15,7 +20,17 @@ class HomeScreen extends StatelessWidget {
             TPrimaryHeaderContainer(
               child: Column(
                 children: [
-                  THomeAppBar()
+                  THomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections,),
+                  TSearchContainer(text: 'Search in store',),
+                  SizedBox(height: TSizes.spaceBtwSections,),
+                  Padding(padding: EdgeInsets.only(left: TSizes.defaultSpace), child: Column(
+                    children: [
+                      TSectionHeading(title: 'Popular Categories', showActionButton: false, textColor: TColors.white,),
+                      SizedBox(height: TSizes.spaceBtwItems,),
+                      THomeCategories()
+                    ],
+                  ),)
                 ],
               ),
                 ),
