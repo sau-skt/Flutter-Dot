26f830b5729d9ecc7caa83236bd54491e99715d2
sau-skt <saurabhchatterjee18@gmail.com>
2024-04-10 11:43:33 -0700

    Custom AppBar Design

--- lib/features/authentication/screens/home/home.dart
+++ lib/features/authentication/screens/home/home.dart
@@ -1,20 +1,23 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'widgets/home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child: Container(),
+              child: Column(
+                children: [
+                  THomeAppBar()
+                ],
+              ),
                 ),
           ],
         ),
