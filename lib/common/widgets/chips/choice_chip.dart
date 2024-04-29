a0ce660ee6f5e93978142d184b5ab3c3b7798164
sau-skt <saurabhchatterjee18@gmail.com>
2024-04-30 03:12:51 +0530

    Product Detail

---
+++ lib/common/widgets/chips/choice_chip.dart
@@ -0,0 +1,37 @@
+import 'dart:isolate';
+
+import 'package:e_commerse/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:e_commerse/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+
+class TChoiceChip extends StatelessWidget {
+  const TChoiceChip(
+      {super.key, required this.text, required this.selected, this.onSelected});
+
+  final String text;
+  final bool selected;
+  final void Function(bool)? onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    final isColor = THelperFunctions.getColor(text) != null ;
+    return Theme(
+      data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+      child: ChoiceChip(
+        label: isColor ? const SizedBox() : Text(text),
+        selected: selected,
+        onSelected: onSelected,
+        labelStyle: TextStyle(color: selected ? TColors.white : null),
+        avatar: THelperFunctions.getColor(text) != null
+        ? TCircularContainer(width: 50, height: 50, backgroundColor: THelperFunctions.getColor(text)!,)
+        : null, 
+        shape: isColor ? const CircleBorder() : null,
+        labelPadding: isColor ? const EdgeInsets.all(0) : null,
+        backgroundColor: isColor ? THelperFunctions.getColor(text)! : null,
+        padding: isColor ? const EdgeInsets.all(0) : null,
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,6 +1,11 @@
+import 'package:e_commerse/common/widgets/texts/section_heading.dart';
+import 'package:e_commerse/features/shop/screens/product_details/widgets/bottom_add_to_cart_widget.dart';
+import 'package:e_commerse/features/shop/screens/product_details/widgets/product_attributes.dart';
 import 'package:e_commerse/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:e_commerse/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:readmore/readmore.dart';
 
 import 'widgets/product_detail_image_slider.dart';
 import 'widgets/rating_share_widget.dart';
@@ -10,17 +15,44 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      bottomNavigationBar: const TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TProductImageSlider(),
+            const TProductImageSlider(),
             Padding(
-              padding: EdgeInsets.only(right: TSizes.defaultSpace, left: TSizes.defaultSpace, bottom: TSizes.defaultSpace),
+              padding: const EdgeInsets.only(right: TSizes.defaultSpace, left: TSizes.defaultSpace, bottom: TSizes.defaultSpace),
               child: Column(
                 children: [
-                  TRatingAndShare(),
-                  TProductMetaData(),
+                  const TRatingAndShare(),
+                  const TProductMetaData(),
+                  const TProductAttributes(),
+                  const SizedBox(height: TSizes.spaceBtwSections,),
+                  SizedBox(width: double.infinity,child: ElevatedButton(onPressed: () {}, child: const Text('Checkout'))),
+                  const SizedBox(height: TSizes.spaceBtwSections,),
+                  const TSectionHeading(title: 'Description', showActionButton: false,),
+                  const SizedBox(height: TSizes.spaceBtwItems,),
+                  const ReadMoreText(
+                    'This is a Product Description for Blue Nike Sleeve less vest. There are more things that can be added but I am things that can be added but I am things that can be added but I am',
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: ' Show more',
+                    trimExpandedText: ' Less',
+                    moreStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
+
+                  const Divider(),
+                  const SizedBox(height: TSizes.spaceBtwItems,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TSectionHeading(title: 'Reviews(199)', onPressed: () {}, showActionButton: false,),
+                      IconButton(onPressed: () {}, icon: const Icon(Iconsax.arrow_right_3, size: 18,),)
+                    ],
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections,),
                 ],
               ),
             ),
